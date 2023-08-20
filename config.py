8216f5a365ab66fa516aa67943a1c6998713ab51
Indianbro67 <133229712+Indianbro67@users.noreply.github.com>
2023-08-20 10:55:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     API_ID = 22894281
     API_HASH = "b59176de6ec96903b816d72c2ad4fff8"
-    TOKEN = "6157850456:AAECSQKBOrjcmF_mI9ngnltvsHqjV7TBOEk"  
+    TOKEN = "6469756582:AAHf6qMbLGGTlPbghhtfMUN24BHpomCih68"  
     START_PIC = "https://telegra.ph/file/8c42a29868e292834161d.jpg" 
     CHAT = ""    
